df95209fc51a45a2e8b983690968c9b5d0c71ab1
fallaha13 <fallaha@wit.edu>
2015-05-28 13:39:00 -0700

    Initial commit

---
+++ ZaJourney/RootViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ZaJourney
+//
+//  Created by alex fallah on 5/28/15.
+//  Copyright (c) 2015 alex fallah. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
